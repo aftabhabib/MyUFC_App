f2ae202c788718c208546f86b2525ab7ded27df4
gaffycool <gaffy_cool@hotmail.com>
2018-03-06 10:32:11 +0000

    starting to implement MVP
    starting to code the app using an MVP methodology

---
+++ app/src/main/java/com/example/tae/myufc_app/data/network/DataManager.java
@@ -0,0 +1,14 @@
+package com.example.tae.myufc_app.data.network;
+
+/**
+ * Created by TAE on 13-Feb-18.
+ */
+
+/**
+ * Extends all three modules
+ */
+
+
+public interface DataManager extends IApiHelper {
+
+}
